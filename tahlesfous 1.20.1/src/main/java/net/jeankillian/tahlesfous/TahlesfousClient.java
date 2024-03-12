305c65eb35b58dbe455f2ae0ff7d3d493a583a35
Jean-Killian <killian.goulet.dubasque@gmail.com>
2024-03-12 16:01:14 +0100

    q

---
+++ tahlesfous 1.20.1/src/main/java/net/jeankillian/tahlesfous/TahlesfousClient.java
@@ -0,0 +1,10 @@
+package net.jeankillian.tahlesfous;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TahlesfousClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
